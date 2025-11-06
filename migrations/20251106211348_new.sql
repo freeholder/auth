b82ae3ffbe3bfcffd01bf6cf8de865b9aa6961a2
Egor Shitov <freeholder101@gmail.com>
2025-11-06 21:57:26 +0300

    database added

---
+++ migrations/20251106211348_new.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+create table users (
+    id serial primary key,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    role integer default 0,
+    created_at timestamp not null,
+    updated_at timestamp default now()
+);
+-- +goose Down
+drop table users;
